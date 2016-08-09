1482c56e653acf93ec2d81351e917b31258b1928
Piotr Wroński <piotr.wronski@impaqgroup.com>
2016-08-09 12:59:25 +0200

    Removed log4j fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,7 @@
-import org.apache.log4j.Logger;
 
 public class Main {
 
-    private final static Logger logger = Logger.getLogger(Main.class);
-
     public static void main(String[] args){
-        logger.info("Program started");
-        logger.info("Method main");
-        m();
-        logger.info("Program stopped");
-
-    }
-
-    private static void m(){
-        logger.info("Method m");
+        System.out.println("Hello world!");
     }
 }
